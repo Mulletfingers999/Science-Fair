39c0911e8f31f31858eb20ff93ab41f30648c521
mulletfinger999 <lpoflynn@gmail.com>
2015-12-12 21:36:33 -0800

    Init

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./css/master.css" media="screen" title="no title" charset="utf-8">
+    <title>Science Fair Project</title>
+  </head>
+  <body>
+    <br>
+    <h1>Analyze Articles</h1>
+    <hr>
+    
+  </body>
+</html>
