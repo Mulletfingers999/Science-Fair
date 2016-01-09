333221c2fe81fc288218fcf3c8d1836c928d1c6a
mulletfinger999 <lpoflynn@gmail.com>
2016-01-09 10:01:55 -0800

    Articles Pushed

--- index.php
+++ index.php
@@ -9,6 +9,39 @@
     <br>
     <h1>Analyze Articles</h1>
     <hr>
-    
-  </body>
+    <table>
+      <thead>
+        <tr>
+          <th>
+            Pick a Scientific Article
+          </th>
+          <th>
+            Results
+          </th>
+          <th>
+            Pick a Denying Article
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <select class="" name="">
+              <option> -- Hannity, Sean</option>
+              <option>"Global Warming: So Dishonest It Makes Enron Look Like a Paragon of Integrity." -- Delingpole, James</option>
+              <option>"New Reports: There Is No Global Warming." -- NewsMax</option>
+            </select>
+          </td>
+          <td>
+            <span class="tiny">[ ... ]</span>
+          </td>
+          <td>
+            <select class="" name="">
+              <option>"The Physical Science Basis: Summary for Policymakers" -- Multiple Authors, Environmental Analyst</option>
+            </select>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    </body>
 </html>
