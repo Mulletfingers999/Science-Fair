0b8d9c386462d8e9c814ef3aab06b5ee49ef3464
mulletfinger999 <lpoflynn@gmail.com>
2016-02-08 19:01:28 -0800

    Minor front-end changes

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <title>Science Fair Project</title>
   </head>
   <body>
+    <a href="https://github.com/whirish/Science-Fair"><img style="position: fixed; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/121cd7cbdc3e4855075ea8b558508b91ac463ac2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png"></a>
     <br>
     <h1 style="color: yellow;">Analyze Articles</h1>
     <hr>
@@ -17,13 +18,13 @@
       <thead>
         <tr>
           <th>
-            Pick a Pro-Global Warming Article
+            Pick an Anti-Global Warming Article
           </th>
           <th>
 
           </th>
           <th>
-            Pick a Anti-Global Warming Article
+            Pick a Pro-Global Warming Article
           </th>
         </tr>
       </thead>
@@ -168,7 +169,7 @@
               18.
             </td>
             <td>
-              Number of occurances of this string in this text
+              Number of occurrences of this string in this text
             </td>
           </tr>
           <tr>
@@ -179,7 +180,7 @@
               18.
             </td>
             <td>
-              Number of occurances of this string in this text
+              Number of occurrences of this string in this text
             </td>
           </tr>
         </table>
@@ -258,7 +259,7 @@
                   }
 
                   echo "</tr>";
-                  //echo "<li value=\"$value\"><span style=\"color:$bgcolor;\">$key</span></li>";//"\n" . $value . " occurences of " . $key;
+                  //echo "<li value=\"$value\"><span style=\"color:$bgcolor;\">$key</span></li>";//"\n" . $value . " occurrences of " . $key;
                 }
               }
             }
